c2ffb0d31b5e5142cd8bf2c01854bdd9cdab5041
Ethan Henrickosn <henricksonethan@gmail.com>
2024-09-24 19:57:03 -0500

    Added Front End Basics
    Added a basic home page

---
+++ frontend/TCG/src/lib/components/card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    
+</script>
+<style>
+    .card{
+        width: 200px;
+        min-height: 300px;
+        border-radius: 1rem;
+        border: 2px solid rgb(71, 71, 71);
+        box-shadow: 10px 10px 20px rgb(180, 180, 180);
+
+        transition: .35s;
+
+    }
+
+    .card:hover {
+        box-shadow: 10px 10px 20px rgb(122, 122, 122);
+
+        transition: .35s;
+
+
+    }
+</style>
+<div class="card">
+
+</div>
---
+++ frontend/TCG/src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+<div class="main">
+    <div class="header">
+        <h1>DND - HearthStone</h1>
+    </div>
+    <div class="gameContent">
+        <slot />
+    </div>
+</div>
+<style>
+    .main {
+        width: 100%;
+        min-width: 100vh;
+    }
+
+    .header{
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        background-color: #213740;
+    }
+
+    h1 {
+        margin: 10px 0px;
+        color: #c9d5de;
+    }
+
+</style>
